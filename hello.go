e9325c3d0ceaaa3e520cefdb45fcfd68e4bf9ae7
Miguel Alpendre <miguel.alpendre@olx.com>
2018-04-20 11:51:41 +0100

    Move experiment to proper pkg

--- hello.go
+++ hello.go
@@ -2,52 +2,12 @@ package main
 
 import (
     "fmt"
+
+    "github.com/go-home/experiments"
 )
 
-type World struct {
-    Hello
-}
-
-func (w *World) Get() string {
-    return fmt.Sprintf("world %s", w.Hello.Get())
-}
-
-type Hello struct {
-    Name string
-
-}
-
-func (h *Hello) Get() string {
-    return h.Name
-}
-
-func (h *Hello) Set(s string) {
-    h.Name = s
-}
-
-
 func main() {
+    fmt.Println("starting...")
 
-    fmt.Println("starting")
-
-    w := &World{}
-    w.Set("Mundo")
-
-    display(w)
-
-    //h := &Hello{Name:"Miguel"}
-    //fmt.Printf("%v", h)
-    //h.Set("Rodrigo")
-
-    //display(h)
-
-}
-
-type entity interface {
-    Get() string
-    Set(string)
-}
-
-func display(e entity)  {
-    fmt.Printf("Hello - %s\n", e.Get())
+    experiments.Http()
 }
